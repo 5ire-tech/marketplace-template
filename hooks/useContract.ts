87bc0d5288b5409858e884c90aeae3bdb566ef22
fullDev9233 <jimyy9211@gmail.com>
2023-02-22 04:42:28 -0600

    replace abi

--- hooks/useContract.ts
+++ hooks/useContract.ts
@@ -1,10 +1,10 @@
 import Web3 from 'web3'
 import { AbiItem } from 'web3-utils'
-import abi from '../contracts/abi.json'
+import NFTMarketplace from '@/artifacts/contracts/NftMarketplace.sol/NFTMarketplace.json'
 
 const contractAddress =
   process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ||
-  '0x1A08b8e1101840740Ec98bc7EBDf4fB538cc52F9'
+  '0xE1F9b6ba798AB0830703Fc964A5FdC4B7d181D1B'
 
 const useContract = () => {
   const web3 = new Web3(Web3.givenProvider)
@@ -12,7 +12,7 @@ const useContract = () => {
   const nftContract = new web3.eth.Contract(
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    abi as unknown as AbiItem,
+    NFTMarketplace.abi as unknown as AbiItem,
     contractAddress,
   )
 
